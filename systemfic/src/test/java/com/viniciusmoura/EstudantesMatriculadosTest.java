de3e24a252c3035916cb29c7a067f9ea140a7aa8
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-12-06 23:46:35 -0300

    Modificando a classe EstudantesMatriculados, adicionnando métodos, e refatoração

--- systemfic/src/test/java/com/viniciusmoura/EstudantesMatriculadosTest.java
+++ systemfic/src/test/java/com/viniciusmoura/EstudantesMatriculadosTest.java
@@ -28,7 +28,7 @@ class EstudantesMatriculadosTest {
     }
 
     @Test
-    @DisplayName("estudatematriculadodeveradiminuirnumerodevagasDisponivies")
+    @DisplayName("estudatematriculadodeveradiminuironumerodevagasDisponivies")
     public void deveraDiminuirVagasDisponiveis() throws Exception{
         List<Estudante> estudantes = new ArrayList<>();
         List<Curso> cursos = new ArrayList<>();
@@ -37,13 +37,64 @@ class EstudantesMatriculadosTest {
         cursos.add(new Curso("Matematica", "40hs", "Curso de exatas"));
         turmaCurso.add(new TurmaCurso("Sala 105", 10, LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), LocalDate.now(), LocalDate.now().plusDays(2), cursos.get(0)));
         estudantes.add(new Estudante("Flavio", "25/05/2006", "308 sul", "gat@gmail.com", "69666485"));
+        //Pega a quantidade de vagas
         int vagasD = turmaCurso.get(0).getVagasDisponiveis();
-        
+        //Estancia estudante
         new EstudanteMatriculados("2022-12-07", estudantes.get(0).getMatricula(), turmaCurso);
+        //Verificando se foi decrementado
         assertTrue(turmaCurso.get(0).getVagasDisponiveis()<vagasD);
 
     }
 
-    
+    @Test
+    @DisplayName("MatricularEstudanteComDataForaDoPeriodoLancaExcecao")
+    public void deveraValidarDataForaDoPeriodo() throws Exception{
+        List<Estudante> estudantes = new ArrayList<>();
+        List<Curso> cursos = new ArrayList<>();
+        List<TurmaCurso> turmaCurso = new ArrayList<>();
+
+        cursos.add(new Curso("Matematica", "40hs", "Curso de exatas"));
+        turmaCurso.add(new TurmaCurso("Sala 105", 10, LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), LocalDate.now(), LocalDate.now().plusDays(2), cursos.get(0)));
+        estudantes.add(new Estudante("Flavio", "25/05/2006", "308 sul", "gat@gmail.com", "69666485"));
+        
+        assertThrowsExactly(java.lang.IllegalArgumentException.class,()->{
+            new EstudanteMatriculados("2022-12-25", estudantes.get(0).getMatricula(), turmaCurso);
+        });
+        
+    }
+
+    @Test
+    @DisplayName("MatricularEstudanteComDataDentroDoPeriodo")
+    public void deveraValidarEstudanteComDataDentroDoPeriodo() throws Exception{
+        List<Estudante> estudantes = new ArrayList<>();
+        List<Curso> cursos = new ArrayList<>();
+        List<TurmaCurso> turmaCurso = new ArrayList<>();
+
+        cursos.add(new Curso("Matematica", "40hs", "Curso de exatas"));
+        turmaCurso.add(new TurmaCurso("Sala 105", 10, LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), LocalDate.now(), LocalDate.now().plusDays(2), cursos.get(0)));
+        estudantes.add(new Estudante("Flavio", "25/05/2006", "308 sul", "gat@gmail.com", "69666485"));
+        
+        assertDoesNotThrow(()->{
+            new EstudanteMatriculados("2022-12-07", estudantes.get(0).getMatricula(), turmaCurso);
+        });
+    }
+
+    @Test
+    @DisplayName("MatricularEstudanteComNumeroDeVagasIgualAZeroLancaExcecao")
+    public void deveraValidarEstudanteComNumeroDeVagasIgualZero() throws Exception{
+        List<Estudante> estudantes = new ArrayList<>();
+        List<Curso> cursos = new ArrayList<>();
+        List<TurmaCurso> turmaCurso = new ArrayList<>();
+
+        cursos.add(new Curso("Matematica", "40hs", "Curso de exatas"));
+        turmaCurso.add(new TurmaCurso("Sala 105", 0, LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), LocalDate.now(), LocalDate.now().plusDays(2), cursos.get(0)));
+        estudantes.add(new Estudante("Flavio", "25/05/2006", "308 sul", "gat@gmail.com", "69666485"));
+        
+        assertThrowsExactly(java.lang.IllegalArgumentException.class,()->{
+            new EstudanteMatriculados("2022-12-07", estudantes.get(0).getMatricula(), turmaCurso);
+        });
+    }
+
+
 
 }
